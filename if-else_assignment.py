783c1c6af908cc22b51d00ca1a3a82362c99b028
murat <mkblvsrs@gmail.com>
2020-07-06 17:52:12 +0200

    My If_else assignment-1

---
+++ if-else_assignment.py
@@ -0,0 +1,6 @@
+name=input("Please write your name to log in  :  ")
+pass_word="W@12"
+if name=="Joseph":
+    print(f"Hello, {name}! The password is  :  {pass_word}")
+else:
+    print(f"Hello, {name}! See you later.")
